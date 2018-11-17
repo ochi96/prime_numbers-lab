c54d7b043218bd69769b8bb68d8ce4c60008b132
ochi96 <44918607+ochi96@users.noreply.github.com>
2018-11-17 22:28:33 +0300

    updated file

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,21 @@
 
+
+def prime_number(guess):
+    if guess==0 or guess==1 or abs(guess)!=guess:
+        return False
+    elif guess>=2:
+        for number in range(2,guess):
+            if (guess%number)==0:
+                return False
+        else:
+            return True
+    else:
+        return True
+
+def primeNumbers(maximum):
+    prime_numbers=[]
+    for number in range(maximum+1):
+        if prime_number(number):
+            prime_numbers.append(number)
+            continue
+    return prime_numbers
